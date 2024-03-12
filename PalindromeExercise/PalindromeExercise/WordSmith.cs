e0518a96b8842542a4bd75b62c7f81ea6e303087
Greg Wanner <greg@tegogroup.com>
2024-03-12 11:53:05 -0400

    Finalize Palindrome exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            bool result = false;
+            string revWord = string.Join("", word.ToCharArray().Reverse());
+            /*
+            string revWord = "";
+            for (int c = word.Length - 1; c >= 0; c--)
+            {
+                revWord += word[c];
+            }
+            */
+            if (word.ToLower() == revWord.ToLower())
+            {
+                result = true;
+            };
+            return result;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,23 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("Racecar", true)]
+        [InlineData("hello", false)]
+        public void IsAPalindromeTest(string word, bool expected)
+        { 
 
+            //Arrange
+            var helper = new WordSmith();
+
+            //Act
+            // call the Add method that is located in the Calculator class
+            // and store its result in a variable named actual
+            var actual = helper.IsAPalindrome(word);
+
+            //Assert
+            //throw new NotImplementedException();
+            Assert.Equal(expected, actual);
         }
     }
 }
